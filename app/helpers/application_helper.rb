e97e987d8c752a6f706d4bfd1f8487965243ddf9
fitumi <pkgsail.dev@gmail.com>
2025-11-19 09:28:34 +0300

    fix people#show backlink

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def back_link_path
+    return params[:return_to] if params[:return_to].present?
+    return request.referer if request.referer.present? && request.referer.include?(request.host)
+    people_path
+  end
 end
